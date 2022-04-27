98b49f4f3854ee369c2443d339fb4cd1f5047d91
Ri Do <70012061+ridohendrawan@users.noreply.github.com>
2022-04-27 13:51:59 +0700

    firstCommit

---
+++ Kosupure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kosupure
+//
+//  Created by Rido Hendrawan on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
