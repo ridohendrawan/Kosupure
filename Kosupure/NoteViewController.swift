b8bf04ee6ddeb4eda4eea36b7cfefc92ea72fec6
Ri Do <70012061+ridohendrawan@users.noreply.github.com>
2022-04-29 16:32:26 +0700

    Finish all form

--- Kosupure/NoteViewController.swift
+++ Kosupure/NoteViewController.swift
@@ -10,17 +10,17 @@ import UIKit
 class NoteViewController: UIViewController {
     
     @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var dateLabel: UILabel!
+//    @IBOutlet var dateLabel: UILabel!
     @IBOutlet var noteLabel: UITextView!
     
     public var noteTitle: String = ""
-    public var date: String = ""
+//    public var date: String = ""
     public var note: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = noteTitle
-        dateLabel.text = date
+//        dateLabel.text = date
         noteLabel.text = note
     }
     
