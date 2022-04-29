b3f8ae4e37ef088b65222b2c63eb4329078776fb
Ri Do <70012061+ridohendrawan@users.noreply.github.com>
2022-04-29 14:01:49 +0700

    Add Event Feature Done (Just Title & Note)

---
+++ Kosupure/EntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EntryViewController.swift
+//  Kosupure
+//
+//  Created by Rido Hendrawan on 27/04/22.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var noteField: UITextView!
+    
+    public var completion: ((String, String) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleField.becomeFirstResponder()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+    }
+    
+    @objc func didTapSave () {
+        if let text = titleField.text, !text.isEmpty, !noteField.text.isEmpty {
+            completion?(text, noteField.text)
+        }
+    }
+
+}
