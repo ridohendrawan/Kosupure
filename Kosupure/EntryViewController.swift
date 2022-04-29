f994d4046854ed0840c587c07277b04686325de9
Ri Do <70012061+ridohendrawan@users.noreply.github.com>
2022-04-29 15:22:01 +0700

    update input date

--- Kosupure/EntryViewController.swift
+++ Kosupure/EntryViewController.swift
@@ -9,21 +9,50 @@ import UIKit
 
 class EntryViewController: UIViewController {
 
-    @IBOutlet var titleField: UITextField!
-    @IBOutlet var noteField: UITextView!
+    @IBOutlet var eventName: UITextField!
+    @IBOutlet var eventDate: UITextField!
+    @IBOutlet var eventNote: UITextView!
     
     public var completion: ((String, String) -> Void)?
     
+    let datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleField.becomeFirstResponder()
+        eventName.becomeFirstResponder()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+        createDatePicker()
     }
     
     @objc func didTapSave () {
-        if let text = titleField.text, !text.isEmpty, !noteField.text.isEmpty {
-            completion?(text, noteField.text)
+        if let text = eventName.text, !text.isEmpty, ((eventDate.text?.isEmpty) != nil), !eventNote.text.isEmpty {
+            completion?(text, eventNote.text)
         }
+    }
+    
+    func createDatePicker() {
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        // bar button
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneBtn], animated: true)
+        //assign toolbar
+        eventDate.inputAccessoryView = toolbar
+        //assign date picker to the text field
+        eventDate.inputView = datePicker
+        //onlyshowdate
+        datePicker.datePickerMode = .date
+    }
+    
+    @objc func donePressed() {
+        //formater
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        eventDate.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
     }
 
 }
--- Kosupure/NoteViewController.swift
+++ Kosupure/NoteViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 class NoteViewController: UIViewController {
     
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
     @IBOutlet var noteLabel: UITextView!
     
     public var noteTitle: String = ""
+    public var date: String = ""
     public var note: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = noteTitle
+        dateLabel.text = date
         noteLabel.text = note
     }
     
